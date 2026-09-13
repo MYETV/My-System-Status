d6058c6b4114c111c41b99c7a8c9ed3e28ef05d1
Oscar Cosimo <OskarCosimo@users.noreply.github.com>
2026-09-13 22:49:41 +0200

    Update application version to 1.0.12

--- config/app.php
+++ config/app.php
@@ -7,7 +7,7 @@ return [
      * The version string is checked against GitHub API tags for auto-updates.
      */
     'name'        => 'My System Status',
-    'version'     => '1.0.11',
+    'version'     => '1.0.12',
 
     /*
      * Debug Mode
