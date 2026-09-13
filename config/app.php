659c58eefad19ebadb66eff84ac9b0ce7ac9356f
Oscar Cosimo <OskarCosimo@users.noreply.github.com>
2026-09-13 04:14:02 +0200

    Update version number to 1.0.8 in app config

--- config/app.php
+++ config/app.php
@@ -7,7 +7,7 @@ return [
      * The version string is checked against GitHub API tags for auto-updates.
      */
     'name'        => 'My System Status',
-    'version'     => '1.0.7',
+    'version'     => '1.0.8',
 
     /*
      * Debug Mode
