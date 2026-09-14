af33d6c4edd2122a05b572a0075b103a804ee61e
Oscar Cosimo <OskarCosimo@users.noreply.github.com>
2026-09-14 04:34:41 +0200

    Update version number to 1.0.24

--- config/app.php
+++ config/app.php
@@ -7,7 +7,7 @@ return [
      * The version string is checked against GitHub API tags for auto-updates.
      */
     'name'        => 'My System Status',
-    'version'     => '1.0.23',
+    'version'     => '1.0.24',
 
     /*
      * Debug Mode
