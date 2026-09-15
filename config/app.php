22a2e4f42b7500098e495f66dde9e7dba93325ef
Oscar Cosimo <OskarCosimo@users.noreply.github.com>
2026-09-15 22:19:04 +0200

    Update application version to 1.0.35

--- config/app.php
+++ config/app.php
@@ -7,7 +7,7 @@ return [
      * The version string is checked against GitHub API tags for auto-updates.
      */
     'name'        => 'My System Status',
-    'version'     => '1.0.34',
+    'version'     => '1.0.35',
 
     /*
      * Debug Mode
